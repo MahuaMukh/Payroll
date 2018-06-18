fbbc50a85ff973fcd13c166055732a56ee65f56e
Mahua Mukherjee <mahua.c.mukherjee@gmail.com>
2018-06-17 21:17:41 -0400

    Add project files.

---
+++ Payroll/Employee.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+namespace Payroll
+{
+    class Employee
+    {
+        private string name;
+        private int hours;
+        private int hourlyPay;
+        private const int SALARY = 1500;
+
+
+
+        public Employee()
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+
+            }
+            set
+            {
+                if (value.Length >= 1 && value.Length <= 10)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        public int Hours
+        {
+            get
+            {
+                return hours;
+
+            }
+            set
+            {
+                if (value >= 1 && value <= 20)
+                {
+                    hours = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+        }
+
+        public int HourlyPay
+        {
+            get
+            {
+                return hourlyPay;
+            }
+            set
+            {
+                if (value >= 1 && value <= 40)
+                {
+                    hourlyPay = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+        }
+
+        public int PaySalary
+        {
+            get
+            {
+                return SALARY;
+            }
+        }
+    }
+
+ }
+
---
+++ Payroll/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace Payroll
+{
+    class Program
+    {
+
+        enum Payroll
+        {
+            Hourly,
+            Salaried
+        };
+        static void Main(string[] args)
+        {
+            //Payroll value = Payroll.Salaried;
+            Payroll value = Payroll.Hourly;
+            Employee employee = new Employee();
+            Employee employee1 = new Employee();
+            employee.Name = "Minty";
+            employee1.Name = "Molly";
+            employee.Hours = 10;
+            employee.HourlyPay = 25;
+
+            if (value == Payroll.Hourly)
+            {
+          
+                Console.WriteLine("Information: {0} worked {1} hours at an hourly rate of ${2}", employee.Name, employee.Hours, employee.HourlyPay);
+                CalcPay(employee.Hours, employee.HourlyPay);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Information: {0} worked as salaried and earned a total of ${1}", employee1.Name, employee.PaySalary);
+                Console.ReadLine();
+            }
+        }
+
+        public static int CalcPay(int hours, int hourlyPay)
+        {
+            int totalEarned = (hours * hourlyPay);
+            Console.WriteLine("This employee earned a total of ${0}", totalEarned);
+            return totalEarned;
+        }
+
+
+
+
+
+
+    }
+
+}
